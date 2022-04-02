0aa29e0d448ad6f6eabd6746963add053a76a0f2
NadymVanSchaik <34303676+NadymVanSchaik@users.noreply.github.com>
2022-04-02 17:08:55 +0200

    Setup connection with RabbitMQ and tested ability to send message

---
+++ app/send.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+
+var amqp = require('amqplib/callback_api');
+const amqpUrl = process.env.AMQP_URL || 'amqp://localhost:5673';
+
+amqp.connect(amqpUrl, function(error0, connection) {
+    if (error0) {
+        throw error0;
+    }
+    connection.createChannel(function(error1, channel) {
+        console.log(amqpUrl)
+        if (error1) {
+            throw error1;
+        }
+
+        var queue = 'hello';
+        var msg = 'Hello World!';
+
+        channel.assertQueue(queue, {
+            durable: false
+        });
+        channel.sendToQueue(queue, Buffer.from(msg));
+
+        console.log(" [x] Sent %s", msg);
+    });
+    setTimeout(function() {
+        connection.close();
+        process.exit(0);
+    }, 500);
+});
--- app/server.js
+++ app/server.js
@@ -26,3 +26,34 @@ app.listen(config.port, config.host, (e)=> {
     }
     logger.info(`${config.name} running on ${config.host}:${config.port}`);
 });
+
+var amqp = require('amqplib/callback_api');
+const amqpUrl = process.env.AMQP_URL || 'amqp://localhost:5673';
+
+app.get('/sendMessage', (req, res) => {
+    amqp.connect(amqpUrl, function(error0, connection) {
+        if (error0) {
+            throw error0;
+        }
+        connection.createChannel(function(error1, channel) {
+            if (error1) {
+                throw error1;
+            }
+
+            var queue = 'hello';
+            var msg = 'Hello World!';
+
+            channel.assertQueue(queue, {
+                durable: false
+            });
+            channel.sendToQueue(queue, Buffer.from(msg));
+
+            console.log(" [x] Sent %s", msg);
+            res.status(200).send('Message sent ' + msg);
+        });
+        setTimeout(function() {
+            connection.close();
+            process.exit(0);
+        }, 500);
+    });
+});
